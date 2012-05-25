42f8e4387c59c635d0d1cc85658d9fb7e7bd32dc
bing.cheng <cbffr.cheng@gmail.com>
2012-05-25 18:35:01 +0800

    add smoothing presentation scripts

---
+++ java-workspace/bayes/src/main/java/bayes/common/Test.java
@@ -0,0 +1,26 @@
+/**
+ * @author bing.cheng
+ * @date 2012-5-25
+ * @project bayesian
+ * @file Test.java
+ *
+ */
+
+
+package bayes.common;
+
+import java.util.Calendar;
+
+public class Test {
+
+	/**
+	 * @param args
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.out.println(Calendar.getInstance().getTime());
+		Thread.sleep(100000);
+	}
+
+}
